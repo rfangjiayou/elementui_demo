3d1cf758bbe1e5e73903f79b80e8662032e3dc59
fangrui <fangrui@360.cn>
2020-06-29 17:02:12 +0800

    新增toolbar的title

--- src/components/anotherEditor/index.vue
+++ src/components/anotherEditor/index.vue
@@ -77,7 +77,31 @@ export default {
     },
     data() {
         return {
-            localValue: ''
+            localValue: '',
+            titleConfig: {
+                'ql-bold': '加粗',
+                'ql-color': '颜色',
+                'ql-font': '字体',
+                'ql-code': '插入代码',
+                'ql-italic': '斜体',
+                'ql-link': '添加链接',
+                'ql-background': '背景颜色',
+                'ql-size': '字体大小',
+                'ql-strike': '删除线',
+                'ql-script': '上标/下标',
+                'ql-underline': '下划线',
+                'ql-blockquote': '引用',
+                'ql-header': '标题',
+                'ql-indent': '缩进',
+                'ql-list': '列表',
+                'ql-align': '文本对齐',
+                'ql-direction': '文本方向',
+                'ql-code-block': '代码块',
+                'ql-formula': '公式',
+                'ql-image': '图片',
+                'ql-video': '视频',
+                'ql-clean': '清除字体样式'
+            }
         }
     },
     computed: {
@@ -105,10 +129,20 @@ export default {
             }
         }
     },
+    watch: {
+        value: {
+            handler(val) {
+                this.localValue = val;
+            },
+            deep: true
+        }
+    },
     methods: {
         init() {
             const quill = this.$refs[`myQuillEditor-${this.uploadBtnId}`].quill;
             quill.root.addEventListener('paste', this.handlePaste, false);
+
+            this.addQuillTitle()
         },
         handlePaste(evt) {
             if (evt.clipboardData && evt.clipboardData.files && evt.clipboardData.files.length) {
@@ -159,11 +193,8 @@ export default {
         },
         handleChange(...args) {
             const html = args && args[0] && args[0].html;
-            const text = args && args[0] && args[0].text;
             this.localValue = html; // 绑定当前逐渐地值
             this.$emit('change', this.localValue); // 将内容同步到父组件中
-            // 检测字数
-            this.vaildTextLength(text);
         },
         vaildTextLength(value) {
             const text = value ? value : this.$refs[[`myQuillEditor-${this.uploadBtnId}`]].quill.getText()
@@ -205,11 +236,41 @@ export default {
                 })
             }
         },
-        onEditorBlur() {
+        onEditorBlur(...args) {
+            const text = args && args[0] && args[0].text;
+            this.$emit('blur', this.formProp)
+            // 检测字数
+            this.vaildTextLength(text);
         },
         onEditorFocus() {
         },
         onEditorReady() {
+        },
+        addQuillTitle() {
+            const _this = this;
+            const oToolBar = document.querySelectorAll('.ql-toolbar');
+            oToolBar.forEach(element => {
+                const aButton = element.querySelectorAll('button');
+                const aPicker =  element.querySelectorAll('.ql-formats .ql-picker');
+                aButton.forEach(function (item) {
+                    if(item.className === 'ql-script') {
+                        item.value === 'sub' ? item.title = '下标' : item.title = '上标';
+                    }else if(item.className === 'ql-indent') {
+                        item.value === '+1' ? item.title = '向右缩进' : item.title = '向左缩进';
+                    }else{
+                        item.title = _this.titleConfig[item.classList[0]];
+                    }
+                });
+                aPicker.forEach(function (item) {
+                    if (Array.from(item.classList).indexOf('ql-color') > -1 ||
+                        Array.from(item.classList).indexOf('ql-background') > -1 ||
+                        Array.from(item.classList).indexOf('ql-align') > -1) {
+                        const options = item.querySelector('.ql-picker-options')
+                        options.title = '';
+                    }
+                    item.title = _this.titleConfig[item.classList[0]];
+                });
+            });
         }
     },
     components: {
@@ -219,7 +280,7 @@ export default {
         this.init()
     },
     beforeDestroy() {
-        const quill = this.$refs[[`myQuillEditor-${this.uploadBtnId}`]].quill;
+        const quill = this.$refs[`myQuillEditor-${this.uploadBtnId}`].quill;
         quill.root.removeEventListener('paste', this.handlePaste, false);
     }
 }
